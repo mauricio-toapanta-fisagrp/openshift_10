2ccac92ac81d3823343e06f0d5171570df53fd67
mauricio-toapanta-fisagrp <mauricio.toapanta@fisagrp.com>
2025-10-24 18:23:13 -0500

    conexion

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# app_b.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hola Mundo', 200
+    return 'Hola desde app 10', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
