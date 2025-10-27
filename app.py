23a970bc4699bad8e7066cea5731bb6fe86d4ebe
mauricio-toapanta-fisagrp <mauricio.toapanta@fisagrp.com>
2025-10-27 18:23:07 -0500

    errores10

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hola desde app 10', 200
+@app.route('/health', methods=['GET'])
+def call_health():
+    return 'OK', 200
 
+@app.route('/startup', methods=['GET'])
+def call_startup():
+    return 'OK', 200
+    
+@app.route('/readiness', methods=['GET'])
+def call_readiness():
+    return 'OK', 200
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
